ae6c3146dc99bbe9342ad1dec4d63506eff020c0
42salim42 <salim_sayar@hotmail.com>
2025-01-24 23:44:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".overons").click(function(){
+      window.location.href = "webpages/overons.html"; // Vervang "/offerte" door het pad van de gewenste pagina op je eigen website
+    });
+  });
+
+  $(document).ready(function(){
+    $(".diensten").click(function(){
+      window.location.href = "webpages/diensten.html"; // Vervang "/offerte" door het pad van de gewenste pagina op je eigen website
+    });
+  });
